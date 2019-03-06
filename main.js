35599730712731362d4329977554ec4bef4e6d0c
gdemirev01 <gdemirev78@gmail.com>
2019-03-06 12:39:55 +0200

    asdf

---
+++ main.js
@@ -0,0 +1,41 @@
+var id = 1;
+
+function incId() {
+  id++;
+  if(id > 3) {id = 1;}
+}
+function decId() {
+  id--;
+  if(id < 1) {id = 3;}
+}
+
+function showImage(id, show) {
+  if(id > 3) {id = 1;}
+  if(id < 1) {id = 3;}
+
+  if(show)
+    $("#slideshow-image-" + id).show();
+  else 
+    $("#slideshow-image-" + id).hide();
+}
+
+
+
+$(document).ready(function() {
+    showImage(1, true);
+    showImage(2, false);
+    showImage(3, false);
+
+  $("#arrow-left").click(function() {
+    decId();
+    showImage(id - 1, false);
+    showImage(id, true);
+    showImage(id + 1, false);
+  })
+  $("#arrow-right").click(function() {
+    incId();
+    showImage(id - 1, false);
+    showImage(id, true);
+    showImage(id + 1, false);
+  })
+})
